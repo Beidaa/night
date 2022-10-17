c85d7f3ad21dbfea2292ddccf4e0d8aed5b9c2dd
Beidaa <71646528+Beidaa@users.noreply.github.com>
2022-10-17 21:49:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -104,8 +104,19 @@ def get_birthday(birthday, year, today):
     return birth_day
  
  
+# def get_ciba():
+#     url = "http://open.iciba.com/dsapi/"
+#     headers = {
+#         'Content-Type': 'application/json',
+#         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+#                       'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
+#     }
+#     r = get(url, headers=headers)
+#     note_en = r.json()["content"]
+#     note_ch = r.json()["note"]
+#     return note_ch, note_en
 def get_ciba():
-    url = "http://open.iciba.com/dsapi/"
+    url = "https://api.tianapi.com/tiangou/index?key=4bd63a1e9f1b96e518d2a9857359a3f1"
     headers = {
         'Content-Type': 'application/json',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
@@ -113,11 +124,11 @@ def get_ciba():
     }
     r = get(url, headers=headers)
     note_en = r.json()["content"]
-    note_ch = r.json()["note"]
-    return note_ch, note_en
+    # note_ch = r.json()["note"]
+    return  note_en
  
  
-def send_message(to_user, access_token, region_name, weather, temp, wind_dir, note_ch, note_en):
+def send_message(to_user, access_token, region_name, weather, temp, wind_dir,note_en):
     url = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={}".format(access_token)
     week_list = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"]
     year = localtime().tm_year
@@ -224,12 +235,10 @@ if __name__ == "__main__":
     # 传入地区获取天气信息
     region = config["region"]
     weather, temp, wind_dir = get_weather(region)
-    note_ch = config["note_ch"]
-    note_en = config["note_en"]
-    if note_ch == "" and note_en == "":
+
         # 获取词霸每日金句
-        note_ch, note_en = get_ciba()
+    note_en = get_ciba()
     # 公众号推送消息
     for user in users:
-        send_message(user, accessToken, region, weather, temp, wind_dir, note_ch, note_en)
+        send_message(user, accessToken, region, weather, temp, wind_dir, note_en)
     os.system("pause")
